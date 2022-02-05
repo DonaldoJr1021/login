1888aa360ee7006521553ed2dd000fb150913a78
DonaldoJr1021 <donaldojrps21@gmail.com>
2022-02-04 19:35:40 -0500

    first commit schema login

---
+++ src/app/components/Login/login.component.ts
@@ -0,0 +1,36 @@
+import { Message } from '@angular/compiler/src/i18n/i18n_ast';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: string;
+  passw: string;
+  errorMessage: string;
+  error: boolean;
+
+  constructor() {
+    this.user = "";
+    this.passw = ""
+    this.errorMessage = "";
+    this.error = false;
+   }
+
+   loggin(){
+     if(this.user == "Admin" && this.passw == "123"){
+      alert("Donaldo - Done!!!");
+     }else{
+       this.error = true;
+       this.errorMessage = "User or Password incorrect! Verify your information"
+       setInterval(this.errorMessage, 200);
+     }
+   }
+
+  ngOnInit() {
+  }
+
+}
